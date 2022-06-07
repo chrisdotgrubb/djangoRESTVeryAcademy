9ef499aa46e130a9085872469be8307f79e311b2
chrisdotgrubb <developer.grubb@gmail.com>
2022-06-07 12:06:09 -0400

    Serializer and registration via api

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', include('apps.blog.urls', namespace='blog')),
 	path('api/', include('apps.blog_api.urls', namespace='blog_api')),
-	path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+	path('api/user/', include('apps.user.urls', namespace='user')),
 	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
