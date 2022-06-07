30f2db6b2fc21c251a68f38c6cde4922b1a219a6
chrisdotgrubb <developer.grubb@gmail.com>
2022-06-07 10:23:26 -0400

    Set up JWT and new user model.

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.db import models
+from django.forms import Textarea
 
 from apps.user.models import MyUser
 
 
 class MyUserAdmin(UserAdmin):
-	pass
+	list_display = ('email', 'user_name', 'first_name', 'id', 'is_active', 'is_staff')
+	ordering = ('-start_date',)
+	search_fields = ('email', 'user_name', 'first_name',)
+	fieldsets = (
+		(None, {'fields': ('email', 'user_name', 'first_name', 'id')}),
+		('Personal', {'fields': ('about',)}),
+		('Permissions', {'fields': ('is_staff', 'is_active')}),
+	)
+	readonly_fields = ('email', 'id')
+	formfield_overrides = {
+		models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+	}
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')}
+		 ),
+	)
 
 
 admin.site.register(MyUser, MyUserAdmin)
